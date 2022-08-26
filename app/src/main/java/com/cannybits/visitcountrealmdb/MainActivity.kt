1947fa03ceedd7003f27593a8af82d1aa1d7184d
Nyandago <paulmutator@gmail.com>
2022-08-26 17:46:37 +0300

    MainActivity.kt

--- app/src/main/java/com/cannybits/visitcountrealmdb/MainActivity.kt
+++ app/src/main/java/com/cannybits/visitcountrealmdb/MainActivity.kt
@@ -2,10 +2,15 @@ package com.cannybits.visitcountrealmdb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.realm.Realm
+import io.realm.RealmConfiguration
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
     }
 }
